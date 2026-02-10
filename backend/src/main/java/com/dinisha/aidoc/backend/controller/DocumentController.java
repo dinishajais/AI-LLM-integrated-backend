c5e371419ac5c0c73d5811434c336a1bf847148a
eanjiid <eanjiid@E-5CG22757JQ>
2026-02-10 12:57:06 +0530

    add get file logic

--- backend/src/main/java/com/dinisha/aidoc/backend/controller/DocumentController.java
+++ backend/src/main/java/com/dinisha/aidoc/backend/controller/DocumentController.java
@@ -1,12 +1,17 @@
 package com.dinisha.aidoc.backend.controller;
 
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.dinisha.aidoc.backend.entity.Document;
 import com.dinisha.aidoc.backend.response.DocumentUploadResponse;
 import com.dinisha.aidoc.backend.service.DocumentService;
 
@@ -23,6 +28,11 @@ public class DocumentController {
     @PostMapping("/uploads")
     public ResponseEntity<DocumentUploadResponse> upload(@RequestParam("file") MultipartFile file) {
         return ResponseEntity.ok(documentService.upload(file));
+    }
+    
+    @GetMapping("/getFile/{id}")
+    public ResponseEntity<Document> getFileById(@PathVariable UUID id){
+    	return ResponseEntity.ok(documentService.getById(id));
     }
 }
     
